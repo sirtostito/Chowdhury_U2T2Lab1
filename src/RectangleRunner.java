7e2678316070e8364a6216dfe97f082e3036e5b7
BT_2N2_07 <sample@bths.edu>
2023-09-28 13:59:02 -0400

    w/o print commit 2.

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -7,35 +7,13 @@ public class RectangleRunner {
         Rectangle plot2 = new Rectangle(125);
         Rectangle plot3 = new Rectangle();
 
-        System.out.println("Plot 1 length: " + plot1.getLength());
-        System.out.println("Plot 1 width: " + plot1.getWidth());
-        System.out.println("Plot 1 area: " + (plot1.getWidth()*plot1.getLength()));
-        System.out.println(" ");
-        System.out.println("Plot 2 length: " + plot2.getLength());
-        System.out.println("Plot 2 width: " + plot2.getWidth());
-        System.out.println("Plot 2 area: " + (plot2.getWidth()*plot2.getLength()));
-        System.out.println(" ");
-        System.out.println("Plot 3 length: " + plot3.getLength());
-        System.out.println("Plot 3 width: " + plot3.getWidth());
-        System.out.println("Plot 3 area: " + (plot3.getWidth()*plot3.getLength()));
-        System.out.println(" ");
-
         plot1.setWidth(75);
         plot2.setLength(75);
         plot2.setWidth(75);
         plot3.setWidth(75);
 
-        System.out.println("Plot 1 length: " + plot1.getLength());
-        System.out.println("Plot 1 width: " + plot1.getWidth());
-        System.out.println("Plot 1 area: " + (plot1.getWidth()*plot1.getLength()));
-        System.out.println(" ");
-        System.out.println("Plot 2 length: " + plot2.getLength());
-        System.out.println("Plot 2 width: " + plot2.getWidth());
-        System.out.println("Plot 2 area: " + (plot2.getWidth()*plot2.getLength()));
-        System.out.println(" ");
-        System.out.println("Plot 3 length: " + plot3.getLength());
-        System.out.println("Plot 3 width: " + plot3.getWidth());
-        System.out.println("Plot 3 area: " + (plot3.getWidth()*plot3.getLength()));
+        int totalArea = plot1.calculateArea()+plot2.calculateArea()+plot3.calculateArea();
+        System.out.println("These three plots require " + totalArea + " square feet of seeds.");
 
     }
 }
